f15104344942d59707c6f4703aff115d7f830527
abdalla230 <abdalla7ali@icloud.com>
2025-12-19 00:08:14 +0200

    create function for fetch Games Details and  initialState steps during fetch Data

--- src/store/sclices/games-slice.jsx
+++ src/store/sclices/games-slice.jsx
@@ -18,3 +18,61 @@ export const fetchData = createAsyncThunk('fetchData', async ({ platform = "all"
     console.log(data);
     return data
 })
+
+export const getGameDetails = createAsyncThunk('getGameDetails', async (gameId) => {
+    let { data } = await axios.get(`https://free-to-play-games-database.p.rapidapi.com/api/game`, {
+        headers: {
+            'X-RapidAPI-Key': XRapidAPIKey,
+            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        },
+        params: {
+            id: gameId
+        }
+    })
+    console.log(data);
+    
+    return data
+})
+
+const initialState = {
+    message: "",
+    loading: false,
+    error: "",
+    data: [],
+    game: ""
+}
+
+const gamesSlice = createSlice({
+    name: "game",
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            // ------------------------get Games ---------------------------
+            .addCase(fetchData.pending, (state, { payload }) => {
+                state.loading = true
+            })
+            .addCase(fetchData.fulfilled, (state, { payload }) => {
+                state.loading = false
+                state.data = payload
+            })
+            .addCase(fetchData.rejected, (state, { payload }) => {
+                state.loading = false
+                state.error = payload
+            })
+            .addCase(getGameDetails.pending, (state, { payload }) => {
+                state.loading = true
+            })
+            .addCase(getGameDetails.fulfilled, (state, { payload }) => {
+                state.loading = false
+                state.game = payload
+            })
+            .addCase(getGameDetails.rejected, (state, { payload }) => {
+                state.loading = false
+                state.error = payload
+            })
+    }
+})
+
+
+
+export default gamesSlice.reducer

fce2b8f6ae98225b4680c4194eb716552c5f0452
Mohamed-Hany1907 <mohamed23017738@gmail.com>
2025-12-19 01:22:47 +0200

    add new scroll to Scroll Page

--- src/Components/ScrollToTopBTN/ScrollToTopBTN.jsx
+++ src/Components/ScrollToTopBTN/ScrollToTopBTN.jsx
@@ -20,7 +20,9 @@ const ScrollToTopBTN = () => {
         });
     };
 
-    window.addEventListener('scroll', handleScroll);
+     window.addEventListener('scroll', handleScroll);
+
+    
 
     return visible ? (
         <button className="ScrollToTopBTN border border-second-light rounded-full p-2 text-second-light hover:text-light duration-300 shadow-sm animate-bounce" onClick={handleClick}><MdOutlineKeyboardDoubleArrowUp size={30} /></button>

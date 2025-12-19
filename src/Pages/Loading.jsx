e4f05b4371164b471fa154928b34ada2fb84c30b
Mohamed-Hany1907 <mohamed23017738@gmail.com>
2025-12-19 02:01:14 +0200

    Update Contact Format and Change Loading to another responsive Design

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -2,28 +2,58 @@ import React from 'react';
 
 export default function Loading() {
     return (
-        <div className="loading h-screen w-full flex justify-center items-center bg-gray-900">
-  <svg
-    className="animate-spin -ml-1 mr-3 h-16 w-16 text-indigo-500"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    ></circle>
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-    ></path>
-  </svg>
-</div>
-
+        <div className="loading h-screen w-full flex justify-center items-center">
+            <svg className="pl" viewBox="0 0 128 128" width="128px" height="128px">
+                <defs>
+                    <linearGradient id="pl-grad" x1="0" y1="0" x2="1" y2="1">
+                        <stop offset="0%" stopColor="#000" />
+                        <stop offset="100%" stopColor="#fff" />
+                    </linearGradient>
+                    <mask id="pl-mask">
+                        <rect x="0" y="0" width="128" height="128" fill="url(#pl-grad)" />
+                    </mask>
+                </defs>
+                <g strokeLinecap="round" strokeWidth="8" strokeDasharray="32 32">
+                    <g stroke="hsl(253, 84%, 88%)">
+                        <line className="pl__line1" x1="4" y1="48" x2="4" y2="80" />
+                        <line className="pl__line2" x1="19" y1="48" x2="19" y2="80" />
+                        <line className="pl__line3" x1="34" y1="48" x2="34" y2="80" />
+                        <line className="pl__line4" x1="49" y1="48" x2="49" y2="80" />
+                        <line className="pl__line5" x1="64" y1="48" x2="64" y2="80" />
+                        <g transform="rotate(180,79,64)">
+                            <line className="pl__line6" x1="79" y1="48" x2="79" y2="80" />
+                        </g>
+                        <g transform="rotate(180,94,64)">
+                            <line className="pl__line7" x1="94" y1="48" x2="94" y2="80" />
+                        </g>
+                        <g transform="rotate(180,109,64)">
+                            <line className="pl__line8" x1="109" y1="48" x2="109" y2="80" />
+                        </g>
+                        <g transform="rotate(180,124,64)">
+                            <line className="pl__line9" x1="124" y1="48" x2="124" y2="80" />
+                        </g>
+                    </g>
+                    <g stroke="hsl(32, 100%, 50%)" mask="url(#pl-mask)">
+                        <line className="pl__line1" x1="4" y1="48" x2="4" y2="80" />
+                        <line className="pl__line2" x1="19" y1="48" x2="19" y2="80" />
+                        <line className="pl__line3" x1="34" y1="48" x2="34" y2="80" />
+                        <line className="pl__line4" x1="49" y1="48" x2="49" y2="80" />
+                        <line className="pl__line5" x1="64" y1="48" x2="64" y2="80" />
+                        <g transform="rotate(180,79,64)">
+                            <line className="pl__line6" x1="79" y1="48" x2="79" y2="80" />
+                        </g>
+                        <g transform="rotate(180,94,64)">
+                            <line className="pl__line7" x1="94" y1="48" x2="94" y2="80" />
+                        </g>
+                        <g transform="rotate(180,109,64)">
+                            <line className="pl__line8" x1="109" y1="48" x2="109" y2="80" />
+                        </g>
+                        <g transform="rotate(180,124,64)">
+                            <line className="pl__line9" x1="124" y1="48" x2="124" y2="80" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        </div>
     );
 }

a9784915d6c3c778c0c68f830d44aa7a0262aa54
Ibrahim74-hub <ibrahim23017792@gmail.com>
2025-12-19 02:19:56 +0200

    ReName Folder Slices and update Import

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, {  useState , useEffect  } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Game from '../Components/Game/Game'
 import Loading from './Loading'
-import { fetchData } from '../store/slices/games-slice'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { MdExpandMore } from "react-icons/md"
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { fetchData } from '../store/slices/games-slice'
+
 
 
 const Games = () => {
@@ -12,19 +13,20 @@ const Games = () => {
     const data = useSelector((state) => state.games)
     const dispatch = useDispatch()
     useEffect(() => {
-        if (data.data.length === 0) {
-            dispatch(fetchData({ platform: "all" }))
+        if (data.data.length === 0) { 
+            dispatch(fetchData({ platform: "all" }))  
         }
     }, [dispatch, data.data.length])
 
     return (
+
         <HelmetProvider>
             <Helmet>
                 <title>Explore Games</title>
             </Helmet>
             <section className='pt-14'>
                 <div className="container py-5 ">
-
+                    
                     {!data?.loading ? <div className="">
                         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-5 items-center justify-center mt-5">
                             {data?.data?.slice(0, more).map((game, i) => (

78aa1712fca33e301d5fabc26883929ed2581e15
omar_dagher21 <omarsameh6890@gmail.com>
2025-12-19 06:13:48 -0800

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
-import GameDetails from "./Pages/GameDetails";
-import NotFound from "./Pages/NotFound";
-import Contact from "./Pages/Contact";
-import About from "./Pages/About";
 import Games from "./Pages/Games";
-function App() {
+import GameDetails from "./Pages/GameDetails";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import NotFound from "./Pages/NotFound";
 
-  const routes = createBrowserRouter([
-    {
-      path: "/", element: <Layout />, children: [
-        { index: true, element: <Home /> },
-        { path: "/game/:gameId", element: <GameDetails /> },
-        { path: "/games", element: <Games /> },
-        { path: "/about", element: <About /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "*", element: <NotFound /> },
-      ]
-    }
-  ])
+const App = () => {
 
-  return (
-    <>
-      <div>
-      <RouterProvider router={routes} />
-    </div>
-    </>
-  )
-}
+    const routerConfig = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                { index: true, element: <Home /> },
+                { path: "games", element: <Games /> },
+                { path: "game/:gameId", element: <GameDetails /> },
+                { path: "about", element: <About /> },
+                { path: "contact", element: <Contact /> },
+                { path: "*", element: <NotFound /> }
+            ]
+        }
+    ]);
 
-export default App
+    return <RouterProvider router={routerConfig} />;
+};
+
+export default App;
